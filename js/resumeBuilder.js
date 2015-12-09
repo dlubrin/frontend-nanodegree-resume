2fc1014003e50de88b15b24684b365193d82c256
Dennis Lubrin <dennis.lubrin@gmail.com>
2015-12-09 08:35:18 -0500

    Name and Role
    Added Name and Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+
+var myName = "Dennis Lubrin";
+var myEmail = "dennis@dennis-lubrin.com";
+var myRole = "Front-End Web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", myName);
+var formattedRole = HTMLheaderRole.replace("%data%", myRole);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
